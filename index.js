0e707c7db1776f517a1897b0e59278528c9ffd94
Ange Yu <angeyuangeyu@gmail.com>
2020-01-15 17:40:14 -0600

    its ugly but it works

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require("inquirer"); //u gotta use this
-const fs = require("fs"); //u gotta use this
+const fs = require("fs"); //u gotta use this (if you want to do things to files)
 const path = require("path"); //idk if i need this, but it joins all given path segments together 
-const util = require("util"); //idk if i need this either
 const axios = require("axios"); //promise https://www.npmjs.com/package/axios
 var pdf = require('html-pdf'); //HTML to PDF converter https://www.npmjs.com/package/html-pdf
 const HTML = require("./generatehtml");
@@ -27,28 +26,39 @@ const user_input = () => {
 
 user_input()
 
-.then(function(res) {
-  // console.log(res);
-  // console.log(res.github, res.color);
-  const username = res.github;
-  const color = res.color;
+.then(function(answers) {
+  const username = answers.github;
+  const color = answers.color;
 
 
   const queryUrl = `https://api.github.com/users/${username}`;
 
-  axios.get(queryUrl).then(function(answer) {
+  axios.get(queryUrl).then(function(res) {
 
   //hey ange, so you used a variable RESPONSE that wasn't defined
   //so let's make one :)
-  const response = answer.data;
-  console.log(answer.data);
-  HTML.generateHTML(color);
-  // pdf.create(HTML.generateHTML(color)).toFile('./index.pdf', function(err, res) {
-  //   if (err) throw console.log(err);
-  //    console.log(res); 
-  //   });
+  const data = {color, ...res.data} //object that holds all of the data
+  pdf.create(HTML.generateHTML(data)).toFile('./index.pdf', function(err, res) {
+    if (err) throw console.log(err);
+     console.log(res); 
+    });
   });
 }).catch(function(err){
   if (err) throw err;
 });
 
+
+//Profile image
+// User name
+// Links to the following:
+
+// User location via Google Maps
+// User GitHub profile
+// User blog
+
+
+// User bio
+// Number of public repositories
+// Number of followers
+// Number of GitHub stars
+// Number of users following
