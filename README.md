54adb3c24724903f6a41feeb2c2eba8520267b91
Ange Yu <angeyuangeyu@gmail.com>
2020-01-14 18:32:45 -0600

    aaaaaa

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # developer_profile_generator
 Unit 09 Node.js and ES6+ Homework: Developer Profile Generator
+
+NPMS used
+https://www.npmjs.com/package/axios
+https://www.npmjs.com/package/html-pdf
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path"); //idk if i need this, but it joins all given path 
 const util = require("util"); //idk if i need this either
 const axios = require("axios"); //promise https://www.npmjs.com/package/axios
 var pdf = require('html-pdf'); //HTML to PDF converter https://www.npmjs.com/package/html-pdf
-const generateHTML = require("./generateHTML");
+const HTML = require("./generatehtml");
 
 //ask for github username and color
 const user_input = () => {
@@ -25,17 +25,30 @@ const user_input = () => {
 }; //user_input
 
 
-user_input
+user_input()
 
-.then(function({github,color}) {
-  console.log(username, color);
+.then(function(res) {
+  // console.log(res);
+  // console.log(res.github, res.color);
+  const username = res.github;
+  const color = res.color;
+
+
   const queryUrl = `https://api.github.com/users/${username}`;
 
   axios.get(queryUrl).then(function(answer) {
+
+  //hey ange, so you used a variable RESPONSE that wasn't defined
+  //so let's make one :)
+  const response = answer.data;
   console.log(answer.data);
-  pdf.create(generateHTML(response, color)).toFile('./index.pdf', function(err, res) {
-    if (err) return console.log(err);
-     console.log(res); 
-    });
+  HTML.generateHTML(color);
+  // pdf.create(HTML.generateHTML(color)).toFile('./index.pdf', function(err, res) {
+  //   if (err) throw console.log(err);
+  //    console.log(res); 
+  //   });
   });
+}).catch(function(err){
+  if (err) throw err;
 });
+
